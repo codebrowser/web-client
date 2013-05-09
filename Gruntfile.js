228831ace15a3f9f3ef865f5f5feb0f3e72f4c44
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2013-05-09 18:51:55 +0300

    Init

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+    /* Configuration */
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+
+            src: {
+
+                src: ['Gruntfile.js', 'src/*.js'],
+                options: {
+                    jshintrc: 'jshint.json'
+                }
+            },
+
+            tests: {
+
+                src: 'spec/*.js',
+                options: {
+                    jshintrc: 'spec/jshint.json'
+                }
+            }
+        },
+
+        jasmine: {
+
+            src : 'src/*.js',
+            options : {
+                specs : 'spec/*-spec.js'
+            }
+        }
+    });
+
+    /* Load tasks */
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+    /* Register tasks */
+
+    grunt.registerTask('test', ['jshint', 'jasmine']);
+    grunt.registerTask('default', 'test');
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# web-client
+
+Web client for Code Browser (HTML5 + CSS3 + JavaScript + Node.js + Grunt).
+
+## Install Dependencies
+
+Install Grunt's command line interface globally if necessary with `npm install -g grunt-cli`. Download development 
+dependencies by running `npm install -d`.
+
+## Run Tests
+
+Run tests with:
+
+    grunt test
