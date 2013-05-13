d2ca40cee49b5d679fd9c64a828b20dbbf3eabba
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2013-05-13 19:35:16 +0300

    Send error body

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var util = require('util');
 var fs = require('fs');
 var static = require('node-static');
+var mustache = require('mustache');
 
 var environment = process.env.NODE_ENV;
 var port = process.env.PORT || 8080;
 var serverOptions = {};
+var errorTemplate = '<html><head><title>{{status}}</title></head><body><center><h1>{{status}}</h1></center><hr></body></html>';
 
 // Default to development environment
 if (!environment) {
@@ -29,7 +31,7 @@ require('http').createServer(function (request, response) {
         // An error occured whilst trying to serve file
         if (error) {
 
-            console.error('[%s %s] - %s', error.status, error.message, request.url);
+            console.error('[%d %s] - %s', error.status, error.message, request.url);
 
             // Serve an error page if one exists (e.g. 404.html), otherwise just return the error status
             fs.exists(util.format('%s/%d.html', fileServer.root, error.status), function (exists) {
@@ -38,6 +40,7 @@ require('http').createServer(function (request, response) {
                     fileServer.serveFile(util.format('/%d.html', error.status), error.status, {}, request, response);
                 } else {
                     response.writeHead(error.status, error.headers);
+                    response.write(mustache.render(errorTemplate, { status: util.format('%d %s', error.status, error.message) }));
                     response.end();
                 }
             });
