69f7165ae1bed05fb140f00dc12cc1ca5082358b
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2013-05-23 20:27:15 +0300

    Separate editor to own div - #15

--- src/views/editor-view.js
+++ src/views/editor-view.js
@@ -2,8 +2,16 @@ codebrowser.views.EditorView = Backbone.View.extend({
 
     initialize: function () {
 
+        this.render();
+    },
+
+    render: function () {
+
+        var template = $('#editor-template').html();
+        $(this.el).html(template);
+
         // Create editor
-        this.editor = ace.edit(this.el);
+        this.editor = ace.edit('editor');
 
         // Configure editor
         config.editor.configure(this.editor);
