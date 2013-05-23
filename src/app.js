e3734b45e375f33a245ee77455e53bba13ef7366
Kenny Heinonen <kenny_heinonen@hotmail.com>
2013-05-23 12:26:14 +0300

    Ace configuration (Configure ACE Editor - #15)

--- src/app.js
+++ src/app.js
@@ -29,9 +29,21 @@ var editor = {
 
     initialize: function() {
         var editor = ace.edit('container');
-        editor.setTheme('ace/theme/twilight');
+        editor.setTheme('ace/theme/terminal');
         editor.getSession().setMode('ace/mode/java');
         editor.getSession().setTabSize(4);
+        editor.setReadOnly(false);  // false to make it editable
+        editor.setFontSize(14);
+
+        editor.setHighlightActiveLine(true); // true by default
+        editor.setShowPrintMargin(true); // true by default
+        editor.getSession().setUseSoftTabs(false); // false by default
+        editor.setShowInvisibles(false); // false by default
+
+        editor.getSession().setUseWrapMode(true);
+        editor.getSession().setWrapLimitRange(120, 120); // line length
+        editor.setPrintMarginColumn(120);
+        console.log(editor.getPrintMarginColumn());
     }
 
 }
