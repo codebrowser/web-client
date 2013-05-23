e393441f02b5fdce2f8e42f62162a3f6b6f73521
Kenny Heinonen <kenny_heinonen@hotmail.com>
2013-05-23 15:41:44 +0300

    Editor config to config.js (Configure ACE Editor - #15)

--- config/config.js
+++ config/config.js
@@ -7,3 +7,40 @@ var config = {
     apiRoot: 'http://t-avihavai.users.cs.helsinki.fi/cb-back/app/'
 
 }
+
+var editor = {};
+
+editor.config = (function() {
+
+    function initialize() {
+        var editor = ace.edit('container');
+        editor.setTheme('ace/theme/github');
+        editor.getSession().setMode('ace/mode/java');
+        editor.getSession().setTabSize(4);
+        editor.setReadOnly(true);
+        editor.setFontSize(14);
+        editor.setDisplayIndentGuides(false);
+
+        editor.getSession().setUseWrapMode(true);
+        editor.getSession().setWrapLimitRange(120, 120); // line length
+        editor.setPrintMarginColumn(120);
+
+        editor.setAnimatedScroll(false); // false by default
+        editor.setHighlightActiveLine(true); // true by default
+        editor.setHighlightSelectedWord(true); // true by default
+        editor.setShowPrintMargin(true); // true by default
+        editor.getSession().setUseSoftTabs(false); // false by default
+        editor.setShowInvisibles(false); // false by default
+        editor.setShowFoldWidgets(true); // true by default
+    }
+
+    function setCode(data) {
+        ace.edit('container').getSession().setValue(data);
+    }
+
+    return {
+        initialize: initialize,
+        setCode: setCode
+    };
+
+})();
--- spec/ace-spec.js
+++
@@ -1,9 +0,0 @@
-describe('Ace editor', function () {
-
-    it('can set data in the view', function () {
-
-//        ace.edit('container');
-
-        expect(true).toBe(true);
-    });
-});
