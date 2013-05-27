75d950eb585666ceb737f36ae4787094abf38f73
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2013-05-27 21:30:37 +0300

    Code style

--- src/models/snapshot.js
+++ src/models/snapshot.js
@@ -2,16 +2,18 @@ codebrowser.models.Snapshot = Backbone.RelationalModel.extend({
 
     urlRoot: config.apiRoot + 'students/1/courses/2/exercises/3/snapshots',
 
-    relations: [{
+    relations: [
 
-        type: Backbone.HasMany,
-        key: 'files',
-        relatedModel: 'codebrowser.models.File',
-        collectionType: 'codebrowser.collections.FileCollection',
-        reverseRelation: {
+        {
+            type: Backbone.HasMany,
+            key: 'files',
+            relatedModel: 'codebrowser.models.File',
+            collectionType: 'codebrowser.collections.FileCollection',
+            reverseRelation: {
 
-            key: 'snapshot'
+                key: 'snapshot'
 
+            }
         }
-    }]
+    ]
 });
