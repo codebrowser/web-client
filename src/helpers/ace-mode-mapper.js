ef2175262dc57021172ad6026dff6ac1efaf593d
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2013-05-30 16:13:19 +0300

    Refactor AceModeMapper

--- src/helpers/ace-mode-mapper.js
+++ src/helpers/ace-mode-mapper.js
@@ -2,16 +2,25 @@ codebrowser.helper.AceModeMapper = {
 
     mode: {
 
-        'java':'java',
-        'js':'javascript',
-        'py':'python'
+        'java': 'java'
+
     },
 
-    getMode: function(fileName) {
+    getMode: function (fileName) {
 
-        var fileType = fileName.split('.');
-        fileType = fileType[fileType.length-1];
+        // Fallback to text
+        var mode = 'text';
 
-        return 'ace/mode/' + this.mode[fileType];
+        // Can determine file extension
+        if (fileName.indexOf('.') !== -1 && fileName.indexOf('.') !== 0) {
+
+            var split = fileName.split('.');
+            var fileExtension = split[split.length - 1];
+
+            // Set mode or fallback to text if no mode is specified for file extension
+            mode = this.mode[fileExtension] || mode;
+        }
+
+        return 'ace/mode/' + mode;
     }
 }
