2860d7b8fd816e25b950d1c33cfa8f4780dbb084
Kenny Heinonen <kenny_heinonen@hotmail.com>
2013-06-03 18:09:59 +0300

    Cleaning

--- src/helpers/date-formatter.js
+++ src/helpers/date-formatter.js
@@ -2,8 +2,3 @@ Handlebars.registerHelper('convert', function (time) {
 
     return new Date(time).toLocaleString();
 });
-
-Handlebars.registerHelper('get', function () {
-
-    console.log(this);
-});
