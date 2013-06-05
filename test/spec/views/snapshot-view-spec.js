d1f33169b0495de841111b447d04ae7f79895a36
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2013-06-05 15:53:46 +0300

    Fix

--- test/spec/views/snapshot-view-spec.js
+++ test/spec/views/snapshot-view-spec.js
@@ -4,7 +4,6 @@ describe('Snapshot view', function () {
 
         codebrowser.app.snapshot.navigate('#/students/2/courses/1/exercises/3/snapshots/19');
 
-        expect(codebrowser.app.snapshot.snapshotView).not.toBe(null);
+        expect(codebrowser.app.snapshot.snapshotView.collection).not.toBe(null);
     });
-
 });
