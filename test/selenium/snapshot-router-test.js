a8deb1f4eebb296fb1285173bac2e3aee76d32f3
Kenny Heinonen <kenny_heinonen@hotmail.com>
2013-06-05 14:56:32 +0300

    Tests

--- test/selenium/snapshot-router-test.js
+++ test/selenium/snapshot-router-test.js
@@ -1,4 +1,12 @@
+var driver;
+
 exports.name = 'Snapshot view';
+
+exports.setUp = function () {
+
+    driver = exports.driver;
+    driver.url('localhost:8000/#/students/2/courses/1/exercises/3/snapshots/13');
+}
 
 exports.tests = [
 
@@ -6,57 +14,72 @@ exports.tests = [
         name: 'should render snapshot with correct timestamp',
         func: function (done) {
 
-            exports.driver
-                   .url('localhost:8000/#/students/2/courses/1/exercises/3/snapshots/13')
-                   .getSource(function (source) {
-
-                        buster.assertions.assert(source.indexOf('Code Browser') !== -1);
-
-                    }).end(done);
+                   driver
+                   .waitFor('h2', 10000, function (callback) {
+                        if (callback === false) {
+                            return false;
+                        }
+               
+                        driver.getSource(function (source) {
+                            console.log(source);
+                            buster.assertions.assert(source.indexOf('09:33:34') !== -1);
+                        }).end(done);
+                   });
         }
     },
-
-    /*{
-        name: 'should render snapshot with correct file listing',
-        func: function (done) {
-
-            exports.driver
-                   .url('localhost:8000/#/students/2/courses/1/exercises/3/snapshots/13')
-                   .getSource(function (source) {
-
-                        buster.assertions.assert(source.indexOf('LyyraKortti.java') !== -1);
-                        buster.assertions.assert(source.indexOf('Paaohjelma.java') !== -1);
-
-                    }).end(done);
-        }
-    },*/
 
     {
         name: 'should render snapshot with correct file name',
         func: function (done) {
 
-            exports.driver
-                   .url('localhost:8000/#/students/2/courses/1/exercises/3/snapshots/13')
-                   .getSource( function (source) {
-
-                        buster.assertions.assert(source.indexOf('<h2>LyyraKortti.java</h2>'));
-
-                    }).end(done);
+                   driver
+                   .waitFor('h2', 10000, function (callback) {
+                        if (callback === false) {
+                            return false;
+                        }
+               
+                        driver.getSource(function (source) {
+                            console.log(source);
+                            buster.assertions.assert(source.indexOf('<h2>Paaohjelma.java</h2>') !== -1);
+                        }).end(done);
+                   });
         }
-    }/*,
+    },
 
     {
         name: 'should render correct URLs to files in the list',
         func: function (done) {
 
-            exports.driver
-                   .url('localhost:8000/#/students/2/courses/1/exercises/3/snapshots/13')
-                   .getSource(function (source) {
-
-                        buster.assertions.assert(source.indexOf('<a href="#/students/2/courses/1/exercises/3/snapshots/13/files/15">LyyraKortti.java</a>') !== -1);
-                        buster.assertions.assert(source.indexOf('<a href="#/students/2/courses/1/exercises/3/snapshots/13/files/14">Paaohjelma.java</a>') !== -1);
-
-                    }).end(done);
+                   driver
+                   .waitFor('h2', 10000, function (callback) {
+                        if (callback === false) {
+                            return false;
+                        }
+               
+                        driver.getSource(function (source) {  
+                            buster.assertions.assert(source.indexOf('<a href="#/students/2/courses/1/exercises/3/snapshots/13/files/14">LyyraKortti.java</a>') !== -1);
+                            buster.assertions.assert(source.indexOf('<a href="#/students/2/courses/1/exercises/3/snapshots/13/files/15">Paaohjelma.java</a>') !== -1);
+                        }).end(done);
+                   });
         }
-    }*/
+    },
+
+    {
+        name: 'should render snapshot with correct file listing',
+        func: function (done) {
+
+                   driver
+                   .waitFor('h2', 10000, function (callback) {
+                        if (callback === false) {
+                            return false;
+                        }
+
+                       driver.getSource(function (source) {
+                           console.log(source);
+                           buster.assertions.assert(source.indexOf('LyyraKortti.java') !== -1);
+                           buster.assertions.assert(source.indexOf('Paaohjelma.java') !== -1);
+                        }).end(done);
+                   });
+        }
+    }
 ];
