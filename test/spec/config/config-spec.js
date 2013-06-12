b4ea96567af85ba3e37c5d33c01ea2b476d67dc3
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2013-06-12 13:21:45 +0300

    Add test for EditorView split state storage key

--- test/spec/config/config-spec.js
+++ test/spec/config/config-spec.js
@@ -1,5 +1,10 @@
 describe('Config', function () {
 
+    it('should have correct storage key for EditorView split state', function () {
+
+        expect(config.storage.view.EditorView.split).toBe('codebrowser.view.EditorView.split');
+    });
+
     it('should have correct url root for main API', function () {
 
         expect(config.api.main.root).toBe('http://t-avihavai.users.cs.helsinki.fi/cb-back/app/');
