f8c73fe56d734a61df402c6c6bfe146ab599934c
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2013-06-12 20:06:14 +0300

    Add icon for split button

--- static/assets/js/codebrowser-templates.js
+++ static/assets/js/codebrowser-templates.js
@@ -68,7 +68,7 @@ function program1(depth0,data,depth1) {
   else { stack2 = depth0.files; stack2 = typeof stack2 === functionType ? stack2.apply(depth0) : stack2; }
   if (!helpers.files) { stack2 = blockHelperMissing.call(depth0, stack2, options); }
   if(stack2 || stack2 === 0) { buffer += stack2; }
-  buffer += "\n            </ul>\n\n        </div>\n\n        <button id='split' type='button' class='btn' data-toggle='button'>Split</button>\n\n    </div>\n\n    <div class='span1 text-center'>";
+  buffer += "\n            </ul>\n\n        </div>\n\n        <button id='split' type='button' class='btn' data-toggle='button'><i class='icon-split-editor icon-gray'></i></button>\n\n    </div>\n\n    <div class='span1 text-center'>";
   if (stack2 = helpers.current) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
   else { stack2 = depth0.current; stack2 = typeof stack2 === functionType ? stack2.apply(depth0) : stack2; }
   buffer += escapeExpression(stack2)
