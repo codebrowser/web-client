66c2d2a62ff86806caea835565f581ddb3227965
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2013-06-12 14:29:57 +0300

    Update models and tests

--- static/assets/js/codebrowser.js
+++ static/assets/js/codebrowser.js
@@ -248,11 +248,11 @@ codebrowser.model.File = Backbone.RelationalModel.extend({
 
         return config.api.main.root +
                'students/' +
-               (this.get('snapshot').get('studentId') || this.get('snapshot').collection.studentId) +
+               this.get('snapshot').get('studentId') +
                '/courses/' +
-               (this.get('snapshot').get('courseId') || this.get('snapshot').collection.courseId) +
+               this.get('snapshot').get('courseId') +
                '/exercises/' +
-               (this.get('snapshot').get('exerciseId') || this.get('snapshot').collection.exerciseId) +
+               this.get('snapshot').get('exerciseId') +
                '/snapshots/' +
                this.get('snapshot').id +
                '/files';
