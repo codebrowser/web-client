a4955aa80ad63a4932e8728621660a1fde950627
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2013-06-25 13:11:10 +0300

    Unnecessary hide

--- static/assets/js/codebrowser.js
+++ static/assets/js/codebrowser.js
@@ -633,7 +633,7 @@ codebrowser.view.EditorView = Backbone.View.extend({
         this.editorElement = $('<div>', { id: 'editor' });
 
         // Elements for editors
-        this.sideEditorElement = $('<div>', { id: 'side-editor', height: '800px' }).hide();
+        this.sideEditorElement = $('<div>', { id: 'side-editor', height: '800px' });
         this.mainEditorElement = $('<div>', { id: 'main-editor', height: '800px' });
 
         this.editorElement.append(this.sideEditorElement);
