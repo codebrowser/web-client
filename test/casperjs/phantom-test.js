7374d244d093afb50b2ae5367352fd89a2a70d03
kennyhei <kenny_heinonen@hotmail.com>
2013-07-24 14:54:20 +0300

    jshint for casperjs test files

--- test/casperjs/phantom-test.js
+++ test/casperjs/phantom-test.js
@@ -1,8 +1,8 @@
 // phantom-test.js
 casper.test.begin('PhantomJS homepage has correct title', 1, function suite(test) {
 
-    casper.start("http://www.phantomjs.org", function() {
-        test.assertTitle("PhantomJS: Headless WebKit with JavaScript API", "PhantomJS homepage title is as expected");
+    casper.start('http://www.phantomjs.org', function() {
+        test.assertTitle('PhantomJS: Headless WebKit with JavaScript API', 'PhantomJS homepage title is as expected');
     });
 
     casper.run(function() {
