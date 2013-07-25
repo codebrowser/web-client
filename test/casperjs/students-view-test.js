996bb3f47daba8949f6dd2c80fae7d499608e04e
kennyhei <kenny_heinonen@hotmail.com>
2013-07-25 15:33:01 +0300

    Unbelievable

--- test/casperjs/students-view-test.js
+++ test/casperjs/students-view-test.js
@@ -13,6 +13,7 @@ casper.test.begin('Students view', 3, function suite(test) {
         // Wait for the new page to load given selector
         this.waitForSelector('#students-container', function () {
 
+            this.echo(this.getHTML());
             test.assertSelectorHasText('li.active', 'Students', 'has "Students" label active in the navbar');
             test.assertElementCount('td', 3, 'has three students listed');
             test.assertTruthy(this.getHTML().indexOf('<a href="#/students/7/courses\">student_1003') !== -1, 'has a student with link to course list');
