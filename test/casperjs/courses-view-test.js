0a79287a1993bb5b263a498bccbebb66d2ceebcd
kennyhei <kenny_heinonen@hotmail.com>
2013-07-25 15:15:35 +0300

    Tests

--- test/casperjs/courses-view-test.js
+++ test/casperjs/courses-view-test.js
@@ -4,13 +4,16 @@ casper.test.begin('Courses view', 2, function suite(test) {
 
         this.clickLabel('Courses', 'a');
 
-        // Wait for the new page to load given selector
-        this.waitForSelector('#courses-container');
     });
 
     casper.then(function() {
-        test.assertSelectorHasText('li.active', 'Courses', 'has "Courses" label active in the navbar');
-        test.assertTruthy(this.getHTML().indexOf('<a href="#/courses/1/exercises\">ohpe') !== -1, 'has a course with link to exercise list');
+
+        // Wait for the new page to load given selector
+        this.waitForSelector('#courses-container', function () {
+
+            test.assertSelectorHasText('li.active', 'Courses', 'has "Courses" label active in the navbar');
+            test.assertTruthy(this.getHTML().indexOf('<a href="#/courses/1/exercises\">ohpe') !== -1, 'has a course with link to exercise list');
+        });
     });
 
     casper.run(function() {
---
+++ test/casperjs/students-view-test.js
@@ -0,0 +1,26 @@
+casper.test.begin('Students view', 3, function suite(test) {
+
+    casper.start('http://localhost:8000', function() {
+
+        this.clickLabel('Students', 'a');
+
+    });
+
+    casper.then(function() {
+
+        // Wait for the new page to load given selector
+        this.waitForSelector('#students-container', function () {
+
+            test.assertSelectorHasText('li.active', 'Students', 'has "Students" label active in the navbar');
+            test.assertElementCount('td', 3, 'has three students listed');
+            test.assertTruthy(this.getHTML().indexOf('<a href="#/students/7/courses\">student_1003') !== -1, 'has a student with link to course list');
+        });
+    });
+
+    casper.run(function() {
+
+        test.done();
+        this.echo('');
+
+    });
+});
