36a8a943191e97a40ed4f2086cb64aaf8fc9e872
Kenny Heinonen <kenny_heinonen@hotmail.com>
2013-08-01 11:41:23 +0300

    More tests

--- test/casperjs/students-view-test.js
+++ test/casperjs/students-view-test.js
@@ -1,4 +1,4 @@
-casper.test.begin('Students view', 4, function suite(test) {
+casper.test.begin('Students view', 11, function suite(test) {
 
     casper.start('http://localhost:8000', function() {
 
@@ -21,8 +21,57 @@ casper.test.begin('Students view', 4, function suite(test) {
 
         this.clickLabel('student_423', 'a');
 
+        this.echo('\nstudent_423\n-----------');
+
         this.waitForSelector('#courses-container', function () {
-            test.assertTruthy(this.getHTML().indexOf('<a href="#/students/2/courses/1/exercises">ohpe</a>') !== -1, 'has a course with link to exercise list');
+            test.assertTextExists('ohpe', 'has a course named "ohpe"');
+            test.assertTruthy(this.getHTML().indexOf('<a href="#/students/2/courses/1/exercises">ohpe</a>') !== -1,
+                                                     'course "ohpe" has a correct link to exercise list');
+
+            casper.back();
+        });
+    });
+
+    casper.then(function () {
+
+        this.clickLabel('student_480', 'a');
+
+        this.echo('\nstudent_480\n-----------');
+
+        this.waitForSelector('#courses-container', function () {
+            test.assertTextExists('ohpe', 'has a course named "ohpe"');
+            test.assertTruthy(this.getHTML().indexOf('<a href="#/students/320/courses/1/exercises">ohpe</a>') !== -1,
+                                                     'course "ohpe" has a correct link to exercise list');
+
+            casper.back();
+        });
+    });
+
+    casper.then(function () {
+
+        this.clickLabel('student_13', 'a');
+
+        this.echo('\nstudent_13\n----------');
+
+        this.waitForSelector('#courses-container', function () {
+            test.assertTextExists('mooc-ohja', 'has a course named "mooc-ohja"');
+            test.assertTruthy(this.getHTML().indexOf('<a href="#/students/663/courses/662/exercises">mooc-ohja</a>') !== -1,
+                                                     'course "mooc-ohja" has a correct link to exercise list');
+
+            casper.back();
+        });
+    });
+
+    casper.then(function () {
+
+        this.clickLabel('student_1312', 'a');
+
+        this.echo('\nstudent_1312\n------------');
+
+        this.waitForSelector('#courses-container', function () {
+            test.assertTextExists('mooc-ohja', 'has a course named "mooc-ohja"');
+            test.assertTruthy(this.getHTML().indexOf('<a href="#/students/1932/courses/662/exercises">mooc-ohja</a>') !== -1,
+                                                     'course "mooc-ohja" has a correct link to exercise list');
         });
     });
 
