9620febe118cfd434f1f5c07144e12d454f2fed4
Kenny Heinonen <kenny_heinonen@hotmail.com>
2013-08-07 11:17:46 +0300

    Fix

--- test/spec/views/snapshot-view-spec.js
+++ test/spec/views/snapshot-view-spec.js
@@ -1,9 +1,9 @@
 describe('Snapshot view', function () {
 
-    it('has a non-empty snapshot collection', function () {
+    it('exists when navigating to snapshots', function () {
 
         codebrowser.app.snapshot.navigate('#/students/2/courses/1/exercises/3/snapshots/19');
 
-        expect(codebrowser.app.snapshot.snapshotView.collection).not.toBe(null);
+        expect(codebrowser.app.snapshot.snapshotView).not.toBe(null);
     });
 });
