429ee7ff5a37631f7c4ec88722b0204cf561219e
Kenny Heinonen <kenny_heinonen@hotmail.com>
2013-08-20 15:01:50 +0300

    Diff tests

--- test/spec/models/diff-spec.js
+++ test/spec/models/diff-spec.js
@@ -58,6 +58,31 @@ describe('Diff', function () {
         expect(differences.getDifferences().replace[0].rowEnd).toBe(0);
     });
 
+    it('replaced something to nothing', function () {
+
+        var differences = new codebrowser.model.Diff('Some.\nSomething.', 'Some.\n');
+
+        expect(differences.getDifferences()['delete'].length).toBe(1);
+        expect(differences.getCount()['delete']).toBe(1);
+
+        expect(differences.getDifferences()['delete'][0].type).toBe('delete');
+        expect(differences.getDifferences()['delete'][0].overwrite).toBeTruthy();
+        expect(differences.getDifferences()['delete'][0].rowStart).toBe(1);
+        expect(differences.getDifferences()['delete'][0].rowEnd).toBe(1);
+    });
+
+    it('replaced nothing to something', function () {
+
+        var differences = new codebrowser.model.Diff('\n', '\nHello.');
+
+        expect(differences.getDifferences().insert.length).toBe(1);
+        expect(differences.getCount().insert).toBe(1);
+
+        expect(differences.getDifferences().insert[0].type).toBe('insert');
+        expect(differences.getDifferences().insert[0].rowStart).toBe(1);
+        expect(differences.getDifferences().insert[0].rowEnd).toBe(1);
+    });
+
     it('replace contains deleted lines', function () {
 
         var differences = new codebrowser.model.Diff('Some.\nHello there.\nHow are you?', 'Some.\nHello ');
@@ -76,5 +101,25 @@ describe('Diff', function () {
         expect(differences.getDifferences()['delete'][0].type).toBe('delete');
         expect(differences.getDifferences()['delete'][0].rowStart).toBe(2);
         expect(differences.getDifferences()['delete'][0].rowEnd).toBe(2);
-    })
+    });
+
+    it('replace contains inserted lines', function () {
+
+        var differences = new codebrowser.model.Diff('Some.\nHello there.', 'Some.\nHello there, kiddo.\nGood day! This is\nAwesome.');
+
+        expect(differences.getDifferences().replace.length).toBe(1);
+        expect(differences.getCount().replace).toBe(1);
+
+        expect(differences.getDifferences().insert.length).toBe(1);
+        expect(differences.getCount().insert).toBe(2);
+
+        expect(differences.getDifferences().replace[0].type).toBe('replace');
+        expect(differences.getDifferences().replace[0].overwrite).toBeFalsy();
+        expect(differences.getDifferences().replace[0].rowStart).toBe(1);
+        expect(differences.getDifferences().replace[0].rowEnd).toBe(1);
+
+        expect(differences.getDifferences().insert[0].type).toBe('insert');
+        expect(differences.getDifferences().insert[0].rowStart).toBe(2);
+        expect(differences.getDifferences().insert[0].rowEnd).toBe(3);
+    });
 });
