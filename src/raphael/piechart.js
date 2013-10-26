6cccfc1a3d6f082cf1856783c66a284193498a89
Krista <kristalongi@gmail.com>
2013-10-26 13:18:17 +0300

    Piechart start point and draw direction changed
    Change-Id: I6c81368ce18f21666e3e74e5bb9ad75a84cc81dd

--- src/raphael/piechart.js
+++ src/raphael/piechart.js
@@ -10,16 +10,24 @@ Raphael.fn.pieChart = function (cx, cy, r, values) {
     //Draw a sector
     function sector(cx, cy, r, startAngle, endAngle, params) {
 
-        var x1 = cx + r * Math.cos(-startAngle * rad),
-            x2 = cx + r * Math.cos(-endAngle * rad),
-            y1 = cy + r * Math.sin(-startAngle * rad),
-            y2 = cy + r * Math.sin(-endAngle * rad);
-        return paper.path(['M', cx, cy, 'L', x1, y1, 'A', r, r, 0, +(endAngle - startAngle > 180), 0, x2, y2, 'z']).attr(params);
+        var x1 = cx + r * Math.cos(startAngle * rad),
+            x2 = cx + r * Math.cos(endAngle * rad),
+            y1 = cy + r * Math.sin(startAngle * rad),
+            y2 = cy + r * Math.sin(endAngle * rad);
+
+        return paper.path(['M', cx, cy, 'L', x1, y1, 'A', r, r, 1, +(endAngle - startAngle > 180), 1, x2, y2, 'z']).attr(params);
 
     }
 
-    var angle = 0,
+
+    var angle = -90,
+        total = 0,
         colors = ['#ADE8A5', '#FF9999', '#228BE0', '#FFFFFF'];
+
+    for (var j = 0; j < values.length; j++) {
+
+        total += values[i];
+    }
 
     for (var i = 0; i < values.length; i++) {
 
@@ -36,17 +44,22 @@ Raphael.fn.pieChart = function (cx, cy, r, values) {
         }
 
         if(value === 0) {
+
             continue;
         }
 
         var angleplus = 360 * value / 100,
                 color = colors[i],
                 p = sector(cx, cy, r, angle, angle + angleplus, {fill: color, stroke: '#000', 'stroke-width': 0.5});
+
         p.toBack();
 
         angle += angleplus;
         chart.push(p);
+
     }
 
     return chart;
+    
+
 }
